f2c7956bf04493f5460b0d0d689fe72b293ea63e
Sirawap <45166334+Sirawap@users.noreply.github.com>
2019-04-18 17:09:39 +0700

    main

--- romantest.py
+++ romantest.py
@@ -75,30 +75,4 @@ class KnownValue(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    number = [1,5,10,50,100,500,1000]
-    alpha = ['I','V','X','L','C','D','M']
-    temp = ''
-    for i in range(3999):
-        print(r0.toRoman(i))
-        temp = r0.toRoman(i)
-        print(r0.fromRoman(temp))
-    print('-------------------------')
-    for i in range(3999):
-        print(r1.toRoman(i))
-        temp = r1.toRoman(i)
-        print(r1.fromRoman(temp))
-    print('-------------------------')
-    for i in range(3999):
-        print(r2.toRoman(i))
-        temp = r2.toRoman(i)
-        print(r2.fromRoman(temp))
-    print('-------------------------')
-    for i in range(3999):
-        print(r3.toRoman(i))
-        temp = r3.toRoman(i)
-        print(r3.fromRoman(temp))
-    print('-------------------------')
-    for i in range(3999):
-        print(r4.toRoman(i))
-        temp = r4.toRoman(i)
-        print(r4.fromRoman(temp))
+    unittest.main()
